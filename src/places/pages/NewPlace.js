cd8130972f308a5973ccd25a289ef0a446042569
NhungTranPY <nhungtranpy91@gmail.com>
2023-05-23 16:56:07 +0700

    58. Adding a Custom Form Input Component

--- src/places/pages/NewPlace.js
+++ src/places/pages/NewPlace.js
@@ -1,8 +1,11 @@
 import React from "react";
 
+import Input from "../../shared/components/FormElements/Input";
+import './NewPlace.css'
+
 const NewPlace = () => {
     return (
-        <h2>NewPlace works</h2>
+        <Input element="input" type="text" label="Title"/>
     )
 }
 
