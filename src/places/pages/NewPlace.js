0eeb0684d0fe6b01c481051f87830463daa42007
NhungTranPY <nhungtranpy91@gmail.com>
2023-05-29 17:32:41 +0700

    63. Finishing the "Add Place" Form

--- src/places/pages/NewPlace.js
+++ src/places/pages/NewPlace.js
@@ -49,8 +49,13 @@ const NewPlace = () => {
         dispatch({type: 'INPUT_CHANGE', value: value, isValid: isValid, inputId: id})
     }, [])
 
+    const placeSubmitHandler = event => {
+        event.preventDefault()
+        console.log(formState.inputs); // send this to the backend
+    }
+
     return (
-        <form className="place-form">
+        <form className="place-form" onSubmit={placeSubmitHandler}>
             <Input 
                 id="title"
                 element="input" 
@@ -64,8 +69,16 @@ const NewPlace = () => {
                 id="description"
                 element="textarea"
                 label="Description"
-                validators={[VALIDATOR_REQUIRE(), VALIDATOR_MINLENGTH(5)]}
+                validators={[VALIDATOR_MINLENGTH(5)]}
                 errorText="Please enter a valid description (at least 5 characters)."
+                onInput={inputHandler}
+            />
+            <Input 
+                id="address"
+                element="input"
+                label="Address"
+                validators={[VALIDATOR_REQUIRE()]}
+                errorText="Please enter a valid address)."
                 onInput={inputHandler}
             />
             <Button type="submit" disabled={!formState.isValid}>ADD PLACE</Button>
